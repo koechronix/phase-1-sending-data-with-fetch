1904827dc5329600b7f4a9d122e9fb493ca98129
Amos Koech <koechronix@yahoo.com>
2022-06-16 14:53:18 -0400

    commit tree ff7827ba337fe9cc1a355bd545164432ff295752

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name, email){
+  const userInfo ={name:name, email:email}
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  })
+  .then(response => response.json())
+  .then(userData => {document.body.innerHTML = userData.id})
+  .catch(error => {document.body.innerHTML = error.message})
+}
